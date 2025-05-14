5bf402e42bf2f428a61363612b18f1a0bf4237ab
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-14 19:13:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = "18776104"  # From my.telegram.org
-API_HASH = "80972e25870aea5fc140e78742a91778"  # From my.telegram.org
-BOT_TOKEN = "8091178648:AAHl2JG6ChRVzrdAt_HDHAh2SSjxiJHQEKo"  # From @BotFather
-GOFILE_TOKEN = "gIXnCW50BnmeMvV1Z9GSQe8RPXKkNtYO"  # Your Gofile API token
-MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # MongoDB connection string
+API_ID = ""  # From my.telegram.org
+API_HASH = ""  # From my.telegram.org
+BOT_TOKEN = ":"  # From @BotFather
+GOFILE_TOKEN = ""  # Your Gofile API token
+MONGO_URI = "mongodb+srvnet/?=true&w=majority&appName=Cluster0"  # MongoDB connection string
 ADMIN_ID = 1938030055  # Your Telegram user ID (integer)
-UPDATES_URL = "https://t.me/joinnowearn"  # URL for updates
+UPDATES_URL = "https://t.me/"  # URL for updates
