df4b4e98ea0a61c2e5f087ba5b7aa43b34b99f4f
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-14 16:11:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ BOT_TOKEN = "8091178648:AAHl2JG6ChRVzrdAt_HDHAh2SSjxiJHQEKo"  # From @BotFather
 GOFILE_TOKEN = "gIXnCW50BnmeMvV1Z9GSQe8RPXKkNtYO"  # Your Gofile API token
 MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # MongoDB connection string
 ADMIN_ID = 1938030055  # Your Telegram user ID (integer)
+UPDATES_URL = "https://t.me/joinnowearn"  # URL for updates
